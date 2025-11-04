da0104544c0c01e5182fcd66f506227150096ade
asliddin <asliddinwork@gmail.com>
2025-11-04 23:25:21 +0500

    ....

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/image', '@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/image', '@nuxt/ui'],
 
   ui: {
     colorMode: false,
